88632173177452f09841df637ddf55499bef2d81
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-16 20:37:08 -0700

    Run Python code formatting

--- src/server/runSpeedtest.py
+++ src/server/runSpeedtest.py
@@ -15,14 +15,15 @@ import os
 import sys
 import time
 import traceback
+from typing import Optional
 
 from speedtest import Speedtest
 
 CSV_FIELDNAMES = ["timestamp", "ping", "download", "upload"]
-FILEPATH = os.path.dirname(os.path.abspath(__file__)) + '/../data/result.csv'
+FILEPATH = "%s/../../html/data/result.csv" % os.path.dirname(os.path.abspath(__file__))
 
 
-def findPythonExecutable() -> str:
+def findPythonExecutable() -> Optional[str]:
     """
     In some cases, the `sys.executable` path is not set. This causes problems in some library
     functions like `platform.platform()` which relies on the executable to find the current version
@@ -36,15 +37,11 @@ def findPythonExecutable() -> str:
     """
 
     pythonExecutable = None
-    options = [
-        '/usr/local/bin/',
-        os.path.expanduser('~/local/bin'),
-        '/bin/'
-    ]
+    options = ["/usr/local/bin/", os.path.expanduser("~/local/bin"), "/bin/"]
 
     executables = []
     for option in options:
-        for variant in ['python3.9', 'python3', 'python']:
+        for variant in ["python3.9", "python3", "python"]:
             executables.append(os.path.join(option, variant))
 
     for executable in executables:
@@ -60,67 +57,75 @@ def runSpeedtest() -> int:
     Run speedtest-cli manually by calling directly into API.
     """
 
-    print('--- Starting speedtest ---')
+    print("--- Starting speedtest ---")
 
     returncode = 0
-    servers = []
-    threads = None
 
     # Execute speedtest
     try:
         if not sys.executable:
-            sys.executable = findPythonExecutable()
+            executable = findPythonExecutable()
+
+            if not executable:
+                print("ERROR: Failed to find Python executable.")
+            else:
+                sys.executable = executable
+
             print("Overrode system Python executable: '%s'" % sys.executable)
         else:
             print("System Python executable: '%s'" % sys.executable)
 
         test = Speedtest()
-        test.get_servers(servers)
+        test.get_servers(None)
         test.get_best_server()
-        test.download(threads=threads)
-        test.upload(threads=threads, pre_allocate=False)
+        test.download(threads=None)
+        test.upload(threads=None, pre_allocate=False)
 
         result = test.results.dict()
 
         # collect speedtest data
-        ping = round(result['ping'], 2)
-        download = round(result['download'] / 1000 / 1000, 2)
-        upload = round(result['upload'] / 1000 / 1000, 2)
+        ping = round(result["ping"], 2)
+        download = round(result["download"] / 1000 / 1000, 2)
+        upload = round(result["upload"] / 1000 / 1000, 2)
         timestamp = round(time.time() * 1000, 3)
 
         csv_data_dict = {
             CSV_FIELDNAMES[0]: timestamp,
             CSV_FIELDNAMES[1]: ping,
             CSV_FIELDNAMES[2]: download,
-            CSV_FIELDNAMES[3]: upload}
+            CSV_FIELDNAMES[3]: upload,
+        }
 
         # Write testdata to file
-        isFileEmpty = not os.path.isfile(
-            FILEPATH) or os.stat(FILEPATH).st_size == 0
+        isFileEmpty = not os.path.isfile(FILEPATH) or os.stat(FILEPATH).st_size == 0
 
-        with open(FILEPATH, "a") as f:
+        with open(FILEPATH, "a") as outputFileStream:
             csv_writer = csv.DictWriter(
-                f, delimiter=',', lineterminator='\n', fieldnames=CSV_FIELDNAMES)
+                outputFileStream,
+                delimiter=",",
+                lineterminator="\n",
+                fieldnames=CSV_FIELDNAMES,
+            )
 
             if isFileEmpty:
                 csv_writer.writeheader()
 
             csv_writer.writerow(csv_data_dict)
 
-        # print testdata
-        print('--- Result ---')
+        # Print results from test run
+        print("--- Result ---")
         print("Timestamp: %s" % (timestamp))
         print("Ping: %d [ms]" % (ping))
         print("Download: %d [Mbit/s]" % (download))
         print("Upload: %d [Mbit/s]" % (upload))
-        print('--- Speedtest complete ---')
+        print("--- Speedtest complete ---")
     except Exception:
-        print('---! Speedtest failed !---')
+        print("---! Speedtest failed !---")
         print(traceback.format_exc())
         returncode = 1
 
     return returncode
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(runSpeedtest())
